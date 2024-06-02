6233d9f53532fe3f7188f82d6de216748c160d65
JG-wsb <wrx82014@student.wroclaw.merito.pl>
2024-06-02 20:44:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-# app.py
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
