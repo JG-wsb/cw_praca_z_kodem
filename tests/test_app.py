2d2f7196c4a2884d9275168d0c63d95e9fc6c3fc
JG-wsb <wrx82014@student.wroclaw.merito.pl>
2024-06-02 20:45:44 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,8 @@
-# tests/test_app.py
-import pytest
 from app import app
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
+def test_index():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
 
-def test_hello(client):
-    rv = client.get('/')
-    assert rv.data == b'Hello, World!'
